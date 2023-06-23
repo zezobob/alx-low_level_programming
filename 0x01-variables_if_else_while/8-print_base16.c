34cff6b094933fa3ef8a2f9adf74ab1e314f30f5
zezobob <zezobob_30@yahoo.com>
2023-06-23 14:57:11 -0700

    task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+*main - Entry point A prints all the numbers of base 16 in lowercase
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+char c = 'a';
+int num;
+for (num = 0; num < 16; num++)
+{
+if (num < 10)
+{
+if (num < 10)
+{
+putchar(num + '0');
+c++;
+}
+}
+putchar('\n');
+return  (0);
+}
