5dd1c46244e67d02c9bb07beea1d01af2acbda4d
zezobob <zezobob_30@yahoo.com>
2023-06-23 14:26:05 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+*main - Entry point A prints all 0-9 using putchar
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+int digit = 0;
+while (digit <= 9)
+{
+putchar(digit + '0');
+digit++;
+}
+putchar('\n');
+return (0);
+}
