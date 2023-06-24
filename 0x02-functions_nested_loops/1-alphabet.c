e42e8a04e54dc3cfde81d3cd45f3e6271662c94f
zezobob <zezobob_30@yahoo.com>
2023-06-24 02:12:30 -0700

    a

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include"main.h"
+
+/**
+*main - Entry point A prints  the alphabet, in lowercase
+*Return: Always 0 (Success)
+*/
+void print_alphabet(void)
+{
+int ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+_putchar(ch);
+_putchar('\n');
+}
