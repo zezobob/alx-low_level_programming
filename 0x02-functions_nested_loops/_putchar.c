0f46b4ad0e85afccb49d7cd59f34a74f27857319
zezobob <zezobob_30@yahoo.com>
2023-06-24 00:36:38 -0700

    a

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,6 +1,11 @@
 #include <unistd.h>
 #include "main.h>
 
-int _putchar(char c);
-
-return (0)
+/**
+*main - Entry point A prints the character c to stdout
+*Return: Always 0 (Success)
+*/
+int _putchar(char c)
+{
+return (return(1, &c, 1));
+}
