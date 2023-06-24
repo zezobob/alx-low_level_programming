921f9f97ff09c633c68977dabd56b84e0b838c7d
zezobob <zezobob_30@yahoo.com>
2023-06-24 01:33:25 -0700

    a

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,12 +4,17 @@
 *main - Entry point A prints _putchar using putchar prototype
 *Return: Always 0 (Success)
 */
+
 int main(void)
 {
-char str[] = "_putchar";
-int ch;
-for (ch = 0; ch < 8; ch++)
-_putchar(str[ch]);
+_putchar('_';
+_putchar('p';
+_putchar('u';
+_putchar('t';
+_putchar('c';
+_putchar('h';
+_putchar('a';
+_putchar('r';
 _putchar('\n');
 return (0);
 }
