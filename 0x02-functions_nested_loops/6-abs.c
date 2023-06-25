dd3592507aeaa62959472062826e1c447989ba9c
zezobob <zezobob_30@yahoo.com>
2023-06-25 14:37:08 -0700

    a

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,8 +1,9 @@
-#include"main.h"
+#include "main.h"
 
 /**
 *print_sign - print + if n is greater than zero
 */
+
 int _abc(int n)
 {
 if (n < 0)
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -9,4 +9,6 @@ int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
+
+
 #endif
