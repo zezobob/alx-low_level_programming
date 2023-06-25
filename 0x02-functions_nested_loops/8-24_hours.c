4693f41122c01eb8ebcb2ee7fba14d2c8cbaca57
zezobob <zezobob_30@yahoo.com>
2023-06-25 15:45:34 -0700

    a

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+void jack_bauer(void)
+{
+	int a, b, c, d;
+
+	for (a = 0; a <= 2; a++)
+	{
+	for (b = 0; b <= 9; b++)
+	{
+	if ((a <= 1 && b <= 9) || (a <= 2 && b <= 1))
+	{
+	for (c = 0; c <= 5; c++)
+	{
+	for (d = 0, d <= 9, d++)
+	{
+	_putchar(a + '0');
+	_putchar(b + '0');
+	_putchar(58);
+	_putchar(c + '0');
+	_putchar(d + '0');
+	_putchar('\n');
+	}
+	}
+	}
+	}
+	}
+}
