c6b3fbf2b70dd214776d6e0bd30f8a8ae093dc11
zezobob <zezobob_30@yahoo.com>
2023-06-25 07:29:19 -0700

    a

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include"main.h"
+
+/**
+*print_sign - print + if n is greater than zero
+*/
+
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar(43);
+return (1);
+}
+else if (n == 0)
+{
+_putchar(48);
+return (0);
+}
+else if
+{
+_putchar(45);
+return (-1);
+}
+}
