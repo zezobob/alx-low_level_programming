e5adb8084307246345b135f8fa5e27fb68a4f737
zezobob <zezobob_30@yahoo.com>
2023-06-25 01:30:16 -0700

    a

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
 *print_alphabet_x10 - function that will print the alphabet 10 times
@@ -10,7 +10,7 @@ int line, ch;
 for (line = 0; line <= 9; line++)
 {
 for (ch = 'a'; ch <= 'z'; ch++)
-putchar(ch);
+_putchar(ch);
 _putchar('\n');
 }
 }
