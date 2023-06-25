7a0f88e76013cb61d8ab7ff55d705503fb15aaed
zezobob <zezobob_30@yahoo.com>
2023-06-25 15:23:02 -0700

    a

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,11 +10,13 @@
 
 int print_last_digit(int n)
 {
-	int lastdigit;
+	int last;
 
-	if (n < 0)
-		lastdigit = -1 * (n % 10);
-	else if lastdigit = n % 10;
-	_putchar(lastdigit + '0');
-	return (lastdigit);
+	last = n % 10;
+	if (last  < 0)
+	{
+	last = last * -1;
+	}
+	_putchar(last + '0');
+	return (last);
 }
