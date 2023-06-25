4242439ec42cef901e56c15d44cbf497b272ea35
zezobob <zezobob_30@yahoo.com>
2023-06-25 07:49:29 -0700

    a

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,15 +8,18 @@ int print_sign(int n)
 
 {
 if (n > 0)
-{_putchar(43);
+{
+	_putchar(43);
 return (1);
 }
 else if (n == 0)
-{_putchar(48);
+{
+	_putchar(48);
 return (0);
 }
 else if
-{_putchar(45);
+{
+	_putchar(45);
 return (-1);
 }
 }
