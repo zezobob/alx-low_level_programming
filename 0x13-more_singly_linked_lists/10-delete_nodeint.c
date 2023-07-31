f3acf854011ea89953bc5afd5f611d715d77b46d
zezobob <zezobob_30@yahoo.com>
2023-07-31 12:42:44 -0700

    a

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -10,10 +10,10 @@
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 listint_t *temp = *head;
-listint_t *current = null;
+listint_t *current = NULL;
 unsigned int i = 0;
 
-if (*head == null)
+if (*head == NULL)
 return (-1);
 
 if (index == 0)
@@ -38,5 +38,6 @@ free(current);
 
 return (1);
 }
+
 
 
