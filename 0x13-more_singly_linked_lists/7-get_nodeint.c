f934498b5c3e4b3a79784bae341f36f851a919d7
zezobob <zezobob_30@yahoo.com>
2023-07-31 09:55:27 -0700

    a

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -10,10 +10,13 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 unsigned int i = 0;
 listint_t *temp = head;
+
 while (temp && i < index)
 {
 temp = temp->next;
 i++;
 }
+
 return (temp ? temp : null);
 }
+
