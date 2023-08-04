97053565b32085e8c6e30ece30ebe44d9e847dfc
zezobob <zezobob_30@yahoo.com>
2023-08-04 11:08:44 -0700

    a

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -6,11 +6,12 @@
  * @index: index of the bit to clear
  * Return: 1 for success, -1 for failure
  */
-int clear_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index)
 {
 if (index > 63)
-return  (-1);
+return (-1);
 
 *n = (~(1UL << index) & *n);
 return (1);
 }
+
