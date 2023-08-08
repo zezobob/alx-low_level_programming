7c9d413df006168d4d8f1a5ddab4db09621820a7
zezobob <zezobob_30@yahoo.com>
2023-08-08 10:00:01 -0700

    a

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -11,15 +11,19 @@
 int append_text_to_file(const char *filename, char *text_content)
 {
 int o, w, len = 0;
-if (filename == null)
+
+if (filename == NULL)
 return (-1);
-if (text_content != null)
+
+if (text_content != NULL)
 {
 for (len = 0; text_content[len];)
 len++;
 }
+
 o = open(filename, O_WRONLY | O_APPEND);
 w = write(o, text_content, len);
+
 if (o == -1 || w == -1)
 return (-1);
 close(o);
